cc34ac8cce305d339d25b3a12ecd25a44b716836
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-06-28 15:08:24 -0300

    Added components for heroes

--- firstHeroes/src/app/app.module.ts
+++ firstHeroes/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/heroes/hero-detail/hero-detail.component';
+import { HeroRankingComponent } from './components/heroes/hero-ranking/hero-ranking.component';
+import { HeroEditComponent } from './components/heroes/hero-edit/hero-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    HeroRankingComponent,
+    HeroEditComponent,
   ],
   imports: [
     BrowserModule,
---
+++ firstHeroes/src/app/components/heroes/hero-ranking/hero-ranking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-ranking',
+  templateUrl: './hero-ranking.component.html',
+  styleUrls: ['./hero-ranking.component.scss']
+})
+export class HeroRankingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
