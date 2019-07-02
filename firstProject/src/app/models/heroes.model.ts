04ba670e17829b6aab10a3d93655f590af4777b1
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-02 15:01:08 -0300

    Added heroes models

---
+++ firstProject/src/app/models/heroes.model.ts
@@ -0,0 +1,11 @@
+export class Heroes{
+    id: number;
+    name: String;
+    image: String;
+
+    constructor(id: number, name: String, image: String){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+}
