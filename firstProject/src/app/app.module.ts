306759e2a185ee817f73371226ef7fb2e25bf0c8
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-02 15:01:48 -0300

    Some changes in app component

--- firstProject/src/app/app.module.ts
+++ firstProject/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesListComponent } from './heroes/heroes-list/heroes-list.component';
+import { HeroesEditComponent } from './heroes/heroes-edit/heroes-edit.component';
+import { HeroesDetailComponent } from './heroes/heroes-detail/heroes-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    HeroesComponent,
+    HeroesListComponent,
+    HeroesEditComponent,
+    HeroesDetailComponent
   ],
   imports: [
     BrowserModule,
