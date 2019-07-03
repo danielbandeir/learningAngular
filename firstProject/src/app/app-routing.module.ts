9010e176f0f09e49b7684de24ed40f50caa4dcee
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-03 11:54:29 -0300

    Added edit route

--- firstProject/src/app/app-routing.module.ts
+++ firstProject/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesListComponent } from 'src/app/components/heroes/heroes-list/heroes-list.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroesEditComponent } from './components/heroes/heroes-edit/heroes-edit.component';
 
 const routes: Routes = [
   { path: '', component: HeroesComponent },
-  { path: 'list', component: HeroesListComponent }
+  { path: 'list', component: HeroesListComponent },
+  { path: 'edit/:id', component: HeroesEditComponent }
 ];
 
 @NgModule({
