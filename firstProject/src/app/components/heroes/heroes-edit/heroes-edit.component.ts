6593f57f99d24790d8f1a1297ddb7867c4aec635
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-03 15:32:31 -0300

    Added hero service and get id number by url to edit

--- firstProject/src/app/components/heroes/heroes-edit/heroes-edit.component.ts
+++ firstProject/src/app/components/heroes/heroes-edit/heroes-edit.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroes } from 'src/app/core/models/heroes.model';
+import { HeroesService } from 'src/app/core/services/heroes.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroes-edit',
   templateUrl: './heroes-edit.component.html',
-  styleUrls: ['./heroes-edit.component.scss']
+  styleUrls: ['./heroes-edit.component.scss'],
+  providers: [HeroesService]
 })
 export class HeroesEditComponent implements OnInit {
 
-  constructor() { }
+  hero:Heroes;
+
+  constructor(private heroes: HeroesService, private route: ActivatedRoute) { 
+  }
 
   ngOnInit() {
+    this.hero = this.heroes.getHeroById(Number(this.route.snapshot.paramMap.get('id')));
+    console.log(this.hero.name);
   }
 
 }
