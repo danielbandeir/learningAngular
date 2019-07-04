afd2fe1edecd0d6990982f9ea47d545f2ab6f640
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-04 17:31:01 -0300

    Need fix update hero in controller

--- firstProject/src/app/components/heroes/heroes-edit/heroes-edit.component.ts
+++ firstProject/src/app/components/heroes/heroes-edit/heroes-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Heroes } from 'src/app/core/models/heroes.model';
 import { HeroesService } from 'src/app/core/services/heroes.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes-edit',
@@ -13,6 +14,12 @@ export class HeroesEditComponent implements OnInit {
 
   hero:Heroes;
 
+  editHero = new FormGroup({
+    heroName : new FormControl(''),
+    heroImage : new FormControl(''),
+    heroDescription : new FormControl('')
+  })
+
   constructor(private heroes: HeroesService, private route: ActivatedRoute) { 
   }
 
@@ -20,9 +27,15 @@ export class HeroesEditComponent implements OnInit {
     this.hero = this.heroes.getHeroById(Number(this.route.snapshot.paramMap.get('id')));
   }
 
-  saveHeroDescription(){
+  onSubmit(){
+    this.hero.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.hero.description = this.editHero.get('heroDescription').value;
+    this.hero.name = this.editHero.get('heroName').value;
+    this.hero.image = this.editHero.get('heroImage').value;
+
     this.heroes.editHero(this.hero);
     alert('Editado com sucesso =D');
+  
   }
 
 }
