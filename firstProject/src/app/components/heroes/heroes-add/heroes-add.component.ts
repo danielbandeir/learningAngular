abf5189f966d1f69f4efbddae7af191e674be9f2
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-04 17:30:21 -0300

    Need to fix added new hero to component

--- firstProject/src/app/components/heroes/heroes-add/heroes-add.component.ts
+++ firstProject/src/app/components/heroes/heroes-add/heroes-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/core/services/heroes.service';
 import { Heroes } from 'src/app/core/models/heroes.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -13,6 +14,13 @@ export class HeroesAddComponent implements OnInit {
 
   hero: Heroes;
 
+  heroForm = new FormGroup({
+      //heroId : new FormControl(''),
+      heroName : new FormControl(''),
+      heroImage : new FormControl(''),
+      heroDescription : new FormControl('')
+  });
+
   constructor(private HeroService: HeroesService) {
   }
 
@@ -20,6 +28,14 @@ export class HeroesAddComponent implements OnInit {
   }
 
   addHero(){
+    let getSize: Number;
+    // this.hero.image = this.heroForm.get('heroImage').value;
+    // this.hero.name = this.heroForm.get('heroName').value;
+    // this.hero.description = this.heroForm.get('heroDescription').value;
+    // this.hero.id = Number(this.HeroService.getCountOfHeroes);
+    getSize = Number(this.HeroService.getCountOfHeroes);
+    this.heroForm.get('heroId').setValue(getSize);
+    this.hero = this.heroForm.value;
     this.HeroService.addHero(this.hero);
     alert("Hero adicionado com sucesso =D");
     console.log(this.HeroService.getAllHeroes());
