5f1b2d6cf001e7417791e7eec526b1df2bd3f1fb
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-04 11:22:54 -0300

    Commented constructor

--- firstProject/src/app/core/services/heroes.service.ts
+++ firstProject/src/app/core/services/heroes.service.ts
@@ -8,21 +8,19 @@ export class HeroesService {
   heroes: Heroes[] = [];
 
   constructor() { 
-    this.heroes.push(
-      new Heroes(0, 'Batman', 'https://jovemnerd.com.br/wp-content/uploads/2019/02/the-batman-760x428.jpg', 'sem descrição')
-    );
-    this.heroes.push(
-      new Heroes(1, 'Superman', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV4wC1pes12DV2ck4hwM3bddhpS0d4iKU2maj7AL0SqP61HFaj', 'sem descrição')
-    );
-    this.heroes.push(
-      new Heroes(2, 'Deadpool', 'https://abrilexame.files.wordpress.com/2017/12/deadpool.jpg', 'sem descrição')
-    );
+    //  this.heroes.push(
+    //    new Heroes(0, 'Batman', 'https://jovemnerd.com.br/wp-content/uploads/2019/02/the-batman-760x428.jpg', 'sem descrição')
+    //  );
+    //  this.heroes.push(
+    //    new Heroes(1, 'Superman', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV4wC1pes12DV2ck4hwM3bddhpS0d4iKU2maj7AL0SqP61HFaj', 'sem descrição')
+    //  );
+    //  this.heroes.push(
+    //    new Heroes(2, 'Deadpool', 'https://abrilexame.files.wordpress.com/2017/12/deadpool.jpg', 'sem descrição')
+    //  );
   }
 
   addHero(newHero: Heroes){
-    this.heroes.push(
-      newHero
-    );
+    return this.heroes.push(newHero);
   }
 
   getAllHeroes(){
@@ -54,7 +52,7 @@ export class HeroesService {
   editHero(heroEdited: Heroes){
     for(let i = 0; i<this.heroes.length; i++){
       if(this.heroes[i].id == heroEdited.id){
-        this.heroes[i] == heroEdited;
+        return this.heroes[i] == heroEdited;
       }
     }
   }
