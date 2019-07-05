7e874dc4656d4f669be444ca5f1e65929e146fe6
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 18:18:06 -0300

    Added constructor to client model

--- secondProject/src/app/core/models/client.model.ts
+++ secondProject/src/app/core/models/client.model.ts
@@ -1,6 +1,13 @@
 export class Client{
-    id: String;
+    id: Number;
     name: String;
     requests: String;
     totalRequests: String;
+
+    constructor(id: Number, name: String, requests: String, totalRequests: String){
+        this.id = id;
+        this.name = name;
+        this.requests = requests;
+        this.totalRequests = totalRequests;
+    }
 }
