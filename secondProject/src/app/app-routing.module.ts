4c8f27d8f70e05e22672c242dd39c771ff39b38a
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 16:46:24 -0300

    Added routes

--- secondProject/src/app/app-routing.module.ts
+++ secondProject/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FoodComponent } from './components/food/food.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { RequestsComponent } from './components/requests/requests.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent },
+  { path: '/food', component: FoodComponent },
+  { path: '/clients', component: ClientsComponent },
+  { path: '/requests', component: RequestsComponent }
 ];
 
 @NgModule({
