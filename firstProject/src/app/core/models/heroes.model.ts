8bab350638302bc55f66d60c61c1ad2b8e9c0c91
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 11:08:46 -0300

    Finished model heroes

--- firstProject/src/app/core/models/heroes.model.ts
+++ firstProject/src/app/core/models/heroes.model.ts
@@ -1,6 +1,13 @@
-export interface Heroes{
-    id: number;
+export class Heroes{
+    id: Number;
     name: String;
     image: String;
     description: String;
+
+    constructor(id: Number, name: String, image: String, description: String){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.description = description;
+    }
 }
