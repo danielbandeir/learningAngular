801352a68ddde654458b2a1986157f6ec5dfd786
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 18:17:13 -0300

    Finished food add component methods

--- secondProject/src/app/components/food/food-add/food-add.component.ts
+++ secondProject/src/app/components/food/food-add/food-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Food } from 'src/app/core/models/food.model';
+import { FoodService } from 'src/app/core/service/food.service';
 
 @Component({
   selector: 'app-food-add',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-add.component.scss']
 })
 export class FoodAddComponent implements OnInit {
+  food: Food;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private FoodService: FoodService) { }
+
+  foodAddForm = this.fb.group({
+    foodName : [''],
+    foodImage: [''],
+    foodPrice: ['']
+  });
 
   ngOnInit() {
+  }
+
+  saveFood(){
+    this.food = new Food(this.FoodService.food.length, this.foodAddForm.get('foodName').value, this.foodAddForm.get('foodImage').value, this.foodAddForm.get('foodPrice').value);
+    this.FoodService.addFood(this.food);
   }
 
 }
