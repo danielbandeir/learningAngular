eaee3163fd6bc7f15d2e66861ee9fd96ed879d4e
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 18:18:25 -0300

    Added constructor to food model

--- secondProject/src/app/core/models/food.model.ts
+++ secondProject/src/app/core/models/food.model.ts
@@ -3,4 +3,11 @@ export class Food{
     name: String;
     image: String;
     price: String;
+
+    constructor(id: Number, name: String, image: String, price: String){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+    }
 }
