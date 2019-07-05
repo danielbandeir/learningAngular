c370f4e3d22f8d2a1fa70c92606f98a4b958130e
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 11:31:15 -0300

    Some changes

--- secondProject/src/app/app.module.ts
+++ secondProject/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FoodService } from 'src/app/core/service/food.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [FoodService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
