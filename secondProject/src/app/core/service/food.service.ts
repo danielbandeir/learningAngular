89ec35528c529fdc223265ebb8e40f7fb92e81bd
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 17:48:55 -0300

    Added some function to food service

--- secondProject/src/app/core/service/food.service.ts
+++ secondProject/src/app/core/service/food.service.ts
@@ -8,4 +8,33 @@ export class FoodService {
   food: Food[] = []
 
   constructor() { }
+
+  checkIfHaveFood(){
+    if(this.food.length == 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  addFood(food: Food){
+    return this.food.push(food);
+  }
+
+  deleteFood(food: Food){
+    for(let i=0; i<this.food.length; i++){
+      if(food.id == this.food[i].id){
+        //need remove food
+        this.food.splice(i, 1);
+      }
+    }
+  }
+
+  editFood(food: Food){
+    for(let i=0; i<this.food.length;i++){
+      if(food.id==this.food[i].id){
+        this.food[i] = food;
+      }
+    }
+  }
 }
