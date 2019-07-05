67c42f8fa14167cfa11e80b232184f9ca0ab73c6
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 16:49:03 -0300

    Added requests components

---
+++ secondProject/src/app/components/requests/request-add/request-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-add',
+  templateUrl: './request-add.component.html',
+  styleUrls: ['./request-add.component.scss']
+})
+export class RequestAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ secondProject/src/app/components/requests/request-exclude/request-exclude.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-exclude',
+  templateUrl: './request-exclude.component.html',
+  styleUrls: ['./request-exclude.component.scss']
+})
+export class RequestExcludeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
