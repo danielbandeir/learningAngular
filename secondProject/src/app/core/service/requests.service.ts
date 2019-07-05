23f198e2adaaee6d060eb237eb116e17e0650b1e
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 17:46:54 -0300

    Added functions to request service

--- secondProject/src/app/core/service/requests.service.ts
+++ secondProject/src/app/core/service/requests.service.ts
@@ -8,7 +8,34 @@ export class RequestsService{
     request: Request[] = []
 
     constructor(){
-        
     }
 
+    checkIfHaveRequests(){
+    if(this.request.length == 0){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  addRequest(request: Request){
+    return this.request.push(request);
+  }
+
+  deleteRequest(request: Request){
+    for(let i=0; i<this.request.length; i++){
+      if(request.id == this.request[i].id){
+        //need remove request
+        this.request.splice(i, 1);
+      }
+    }
+  }
+
+  editRequest(request: Request){
+    for(let i=0; i<this.request.length;i++){
+      if(request.id==this.request[i].id){
+        this.request[i] = request;
+      }
+    }
+  }
 }
