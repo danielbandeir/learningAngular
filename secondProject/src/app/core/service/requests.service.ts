07ea039f6335946eb8249bd80fd146f80324611a
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 16:49:18 -0300

    Added requests service

---
+++ secondProject/src/app/core/service/requests.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Request } from 'src/app/core/models/request.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RequestsService{
+    
+
+    constructor(){
+        
+    }
+
+}
