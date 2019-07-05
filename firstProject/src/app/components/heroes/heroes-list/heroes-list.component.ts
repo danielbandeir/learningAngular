9b33e242473f73e4aeab10555dab221b91fca8c9
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 10:16:39 -0300

    Fixed heroes list

--- firstProject/src/app/components/heroes/heroes-list/heroes-list.component.ts
+++ firstProject/src/app/components/heroes/heroes-list/heroes-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from 'src/app/core/services/heroes.service';
+import { Heroes } from 'src/app/core/models/heroes.model';
 
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './heroes-list.component.html',
   styleUrls: ['./heroes-list.component.scss'],
-  providers: [HeroesService]
 })
 export class HeroesListComponent implements OnInit {
+  heroes: Heroes[];
 
-  constructor(private HeroService: HeroesService) { }
+  constructor(private HeroService: HeroesService) { 
+    this.heroes = this.HeroService.heroes;
+  }
 
   ngOnInit() {
   }
