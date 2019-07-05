81beb175eeb2e7b7239b5a612925da8e87dc0eaa
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 15:50:43 -0300

    Added food component

---
+++ secondProject/src/app/components/food/food-exclude/food-exclude.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-exclude',
+  templateUrl: './food-exclude.component.html',
+  styleUrls: ['./food-exclude.component.scss']
+})
+export class FoodExcludeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
