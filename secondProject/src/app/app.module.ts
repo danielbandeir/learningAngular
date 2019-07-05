b57f7c2eb1b74eb5be7dda747134c515a70c2564
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-07-05 15:49:21 -0300

    Added some components

--- secondProject/src/app/app.module.ts
+++ secondProject/src/app/app.module.ts
@@ -6,11 +6,35 @@ import { FoodService } from 'src/app/core/service/food.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientAddComponent } from './components/clients/client-add/client-add.component';
+import { ClientListComponent } from './components/clients/client-list/client-list.component';
+import { ClientListeditComponent } from './components/clients/client-listedit/client-listedit.component';
+import { ClientEditComponent } from './components/clients/client-edit/client-edit.component';
+import { FoodComponent } from './components/food/food.component';
+import { FoodAddComponent } from './components/food/food-add/food-add.component';
+import { FoodListComponent } from './components/food/food-list/food-list.component';
+import { FoodEditComponent } from './components/food/food-edit/food-edit.component';
+import { FoodExcludeComponent } from './components/food/food-exclude/food-exclude.component';
+import { ClientDeleteComponent } from './components/clients/client-delete/client-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientAddComponent,
+    ClientListComponent,
+    ClientListeditComponent,
+    ClientEditComponent,
+    FoodComponent,
+    FoodAddComponent,
+    FoodListComponent,
+    FoodEditComponent,
+    FoodExcludeComponent,
+    ClientDeleteComponent
   ],
   imports: [
     BrowserModule,
